beba24f03e481fb8f9a3288402bbe2526909e918
Dannyblazer <nnajidanny004@gmail.com>
2023-03-13 15:32:37 +0000

    Database models implementation for the private chat feature.

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,60 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+from .models import PrivateChatRoom, RoomChatMessage
+from django.core.paginator import Paginator
+from django.core.cache import cache
+from django.db import models
+
+# Register your models here.
+
+class PrivateChatRoomAdin(admin.ModelAdmin):
+    list_display = ['id', 'user1', 'user2']
+    search_fields = ['id', 'user1', 'user2']
+    readonly_fields = ['id']
+    
+
+    class Meta:
+        model = PrivateChatRoom
+
+admin.site.register(PrivateChatRoom, PrivateChatRoomAdin)
+
+
+# Resource: Http://masnum.rocks/2017/03/20/django-admin-expensive-count-all-queries/
+class CachingPaginator(Paginator):
+    def _get_count(self):
+
+        if not hasattr(self, "_count"):
+            self._count = None
+
+        if self._count is None:
+            try:
+                key = "adm:{0}:count".format(hash(self.object_list.query.__str__()))
+                self._count = cache.get(key, -1)
+                if self._count == -1:
+                    self._count = super().count
+                    cache.set(key, self._count, 3600)
+
+            except:
+                self._count = len(self.object_list)
+        return self._count
+
+    count = property(_get_count)    
+
+
+class RoomChatMessageAdmin(admin.ModelAdmin):
+    list_filter = ['room', 'user', 'timestamp']
+    list_display = ['room', 'user', 'timestamp', 'content']
+    search_fields = ['user', 'content']
+    readonly_fields = ['id', 'user', 'room', 'timestamp']
+
+    show_full_result_count = False
+    paginator = CachingPaginator
+
+    class Meta:
+        model = RoomChatMessage
+
+admin.site.register(RoomChatMessage, RoomChatMessageAdmin)
+
--- chat/models.py
+++ chat/models.py
@@ -1,15 +1,43 @@
 from django.db import models
-from users.models import Account
+from django.conf import settings
 
 # Create your models here.
 
 
-class ChatRoom(models.Model):
-    name = models.CharField(max_length=255)
-    created_at = models.DateTimeField(auto_now_add=True)
+class PrivateChatRoom(models.Model):
+    """
+    Private chat between two users (friends)"""
+    
+    user1       = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="user1")
+    user2       = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="user2")
+    is_active   = models.BooleanField(default=True)
 
-class Message(models.Model):
-    room = models.ForeignKey(ChatRoom, on_delete=models.CASCADE)
-    author = models.ForeignKey(Account, on_delete=models.CASCADE)
-    message = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return "A chat between {} and {}".format(self.user1, self.user2)
+    
+    @property
+    def group_name(self):
+        """ Return the channels group name that sockets should subscribe to receive private chat messages (pair-wise) """
+        
+        return f"PrivateChatRoom-{self.id}"
+    
+class RoomChatMessageManager(models.Manager):
+    def by_room(self, room):
+        qs = RoomChatMessage.objects.filter(room=room).order_by("-timestamp")
+        return qs
+
+class RoomChatMessage(models.Model):
+    """ Chat message created by a user within a room (pair-wise) """
+
+    user        = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    room        = models.ForeignKey(PrivateChatRoom, on_delete=models.CASCADE)
+    timestamp   = models.DateTimeField(auto_now_add=True)
+    content     = models.TextField(unique=False, blank=False)
+
+    objects = RoomChatMessageManager()
+
+    def __st__(self):
+        return self.content
+
+
+
