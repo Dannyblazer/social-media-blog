075a15e195de388ee888c2db9af6a53b48c8c933
Dannyblazer <nnajidanny004@gmail.com>
2023-03-14 10:28:56 +0000

    Edited the 'data-bs-toggle' in the room.html and also added a few constants into constants.py.

--- public_chat/constants.py
+++ public_chat/constants.py
@@ -1,3 +1,9 @@
 DEFAULT_ROOM_CHAT_MESSAGE_PAGE_SIZE = 30 # Public chat message pagination
 MSG_TYPE_MESSAGE = 0 # For standard messages
 MSG_TYPE_CONNECTED_USER_COUNT = 1 # user to initialize the number of connected users for the public chat
+
+MSG_TYPE_ENTER = 0
+MSG_TYPE_LEAVE = 0
+
+def LazyAccountEncoder():
+    pass
