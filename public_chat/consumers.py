87563cadfb15ea9cacfe22d0afdcf19794c1f238
Dannyblazer <nnajidanny004@gmail.com>
2023-03-15 20:40:00 +0000

    Integrated the private chat consumer into the public chat app

--- public_chat/consumers.py
+++ public_chat/consumers.py
@@ -8,6 +8,7 @@ from django.contrib.auth import get_user_model
 from django.utils import timezone
 from django.contrib.humanize.templatetags.humanize import naturalday
 from datetime import datetime
+from chat.models import RoomChatMessage, PrivateChatRoom
 from public_chat.models import PublicChatRoom, PublicRoomChatMessage, PublicRoomChatMessagesManager
 from public_chat.constants import *
 
@@ -276,4 +277,185 @@ class LazyRoomChatMessageEncoder(Serializer):
         dump_object.update({'profile_image': str(obj.user.profile_image.url)})
         dump_object.update({'naturalday': str(calculate_timestamp(obj.timestamp))})
         return dump_object
+
+
+class ChatConsumer(AsyncJsonWebsocketConsumer):
+
+
+	async def connect(self):
+		"""
+		Called when the websocket is handshaking as part of initial connection.
+		"""
+		print("ChatConsumer: connect: " + str(self.scope["user"]))
+
+		# let everyone connect. But limit read/write to authenticated users
+		await self.accept()
+
+		# the room_id will define what it means to be "connected". If it is not None, then the user is connected.
+		self.room_id = None
+
+
+	async def receive_json(self, content):
+		"""
+		Called when we get a text frame. Channels will JSON-decode the payload
+		for us and pass it as the first argument.
+		"""
+		# Messages will have a "command" key we can switch on
+		print("ChatConsumer: receive_json")
+		command = content.get("command", None)
+		try:
+			if command == "join":
+				pass
+			elif command == "leave":
+				# Leave the room
+				pass
+			elif command == "send":
+				pass
+			elif command == "get_room_chat_messages":
+				pass
+			elif command == "get_user_info":
+				pass
+		except Exception as e:
+			pass
+
+
+	async def disconnect(self, code):
+		"""
+		Called when the WebSocket closes for any reason.
+		"""
+		# Leave the room
+		print("ChatConsumer: disconnect")
+		pass
+
+	async def join_room(self, room_id):
+		"""
+		Called by receive_json when someone sent a join command.
+		"""
+		# The logged-in user is in our scope thanks to the authentication ASGI middleware (AuthMiddlewareStack)
+		print("ChatConsumer: join_room: " + str(room_id))
+		
+
+		# Add them to the group so they get room messages
+
+		# Instruct their client to finish opening the room
+
+	async def leave_room(self, room_id):
+		"""
+		Called by receive_json when someone sent a leave command.
+		"""
+		# The logged-in user is in our scope thanks to the authentication ASGI middleware
+		print("ChatConsumer: leave_room")
+
+		
+
+		# Notify the group that someone left
+		pass
+
+		# Remove that we're in the room
+		self.room_id = None
+
+		# Remove them from the group so they no longer get room messages
+		
+		# Instruct their client to finish closing the room
+		
+
+
+	async def send_room(self, room_id, message):
+		"""
+		Called by receive_json when someone sends a message to a room.
+		"""
+		print("ChatConsumer: send_room")
+		# Check they are in this room
+		if self.room_id != None:
+			if str(room_id) != str(self.room_id):
+				print("CLIENT ERRROR 1")
+				raise ClientError("ROOM_ACCESS_DENIED", "Room access denied")
+		else:
+			print("CLIENT ERRROR 2")
+			raise ClientError("ROOM_ACCESS_DENIED", "Room access denied")
+
+		# Get the room and send to the group about it
+		
+
+		# get list of connected_users
+		
+
+		# Execute these functions asychronously
+		
+
+
+	# These helper methods are named by the types we send - so chat.join becomes chat_join
+	async def chat_join(self, event):
+		"""
+		Called when someone has joined our chat.
+		"""
+		# Send a message down to the client
+		print("ChatConsumer: chat_join: " + str(self.scope["user"].id))
+		if event["username"]:
+			await self.send_json(
+				{
+					"msg_type": MSG_TYPE_ENTER,
+					"room": event["room_id"],
+					"profile_image": event["profile_image"],
+					"username": event["username"],
+					"user_id": event["user_id"],
+					"message": event["username"] + " connected.",
+				},
+			)
+
+	async def chat_leave(self, event):
+		"""
+		Called when someone has left our chat.
+		"""
+		# Send a message down to the client
+		pass
+
+
+	async def chat_message(self, event):
+		"""
+		Called when someone has messaged our chat.
+		"""
+		# Send a message down to the client
+		print("ChatConsumer: chat_message")
+
+		pass
+
+	async def send_user_info_payload(self, user_info):
+		"""
+		Send a payload of user information to the ui
+		"""
+		print("ChatConsumer: send_user_info_payload. ")
+		await self.send_json(
+			{
+				"user_info": user_info,
+			},
+		)
+
+	async def display_progress_bar(self, is_displayed):
+		"""
+		1. is_displayed = True
+		- Display the progress bar on UI
+		2. is_displayed = False
+		- Hide the progress bar on UI
+		"""
+		print("DISPLAY PROGRESS BAR: " + str(is_displayed))
+		await self.send_json(
+			{
+				"display_progress_bar": is_displayed
+			}
+		)
+
+
+	async def handle_client_error(self, e):
+		"""
+		Called when a ClientError is raised.
+		Sends error data to UI.
+		"""
+		errorData = {}
+		errorData['error'] = e.code
+		if e.message:
+			errorData['message'] = e.message
+			await self.send_json(errorData)
+		return
+
 
--- public_chat/routing.py
+++
@@ -1,8 +0,0 @@
-# chat/routing.py
-from django.urls import re_path
-
-from public_chat.consumers import PublicChatConsumer
-
-websocket_urlpatterns = [
-    re_path(r"public_chat/$", PublicChatConsumer.as_asgi()),
-]
