b4dc1d82b1a4f3e1765038958717a7b795d31a1a
Dannyblazer <nnajidanny004@gmail.com>
2023-03-16 13:40:39 +0000

    Edited the root route file

--- Blogz/asgi.py
+++ Blogz/asgi.py
@@ -6,7 +6,7 @@ from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 from django.urls import path
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Blogz.settings")
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
