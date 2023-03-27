2f2e27af3952707717be504fac7ad98a0f1096b0
Dannyblazer <nnajidanny004@gmail.com>
2023-03-27 15:09:06 +0000

    Implemented the createchatmessage js function, a submit key handle function and markdown feature.

---
+++ chat/constants.py
@@ -0,0 +1,5 @@
+DEFAULT_ROOM_CHAT_MESSAGE_APGE_SIZE = 20
+
+MSG_TYPE_MESSAGE = 0 # For standard messages
+MSG_TYPE_MESSAGE = 1 # Users enters the room
+MSG_TYPE_MESSAGE = 2 # users leaves the room
--- chat/utils.py
+++ chat/utils.py
@@ -1,4 +1,6 @@
 from chat.models import PrivateChatRoom
+from django.contrib.humanize.templatetags.humanize import naturalday
+from datetime import datetime
 
 def find_or_create_private_chat(user1, user2):
 
@@ -11,3 +13,19 @@ def find_or_create_private_chat(user1, user2):
             chat = PrivateChatRoom(user1=user1, user2=user2)
             chat.save()
     return chat
+
+def calculate_timestamp(timestamp):
+    """
+    Convert the timestamp using humanize to something nice
+    """
+    # Today or Yesterday
+    if ((naturalday(timestamp)=="today") or (naturalday(timestamp)=="yesterday")):
+        str_time = datetime.strftime(timestamp, "%I:%M %p")
+        str_time = str_time.strip("0")
+        ts = f"{naturalday(timestamp)} at {str_time}"
+
+    # Other day
+    else:
+        str_time = datetime.strftime(timestamp, "%m/%d/%Y")
+        ts = f"{str_time}"
+    return ts
