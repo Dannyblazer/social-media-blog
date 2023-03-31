1451f9c27ace34725d353f455087f619327d8e3d
Dannyblazer <nnajidanny004@gmail.com>
2023-03-31 22:13:29 +0000

    Updated the comsumer to retrieve/query chat messages but encountered an error(Will be fixed later).

--- chat/utils.py
+++ chat/utils.py
@@ -1,5 +1,7 @@
 from chat.models import PrivateChatRoom
 from django.contrib.humanize.templatetags.humanize import naturalday
+from django.core.serializers.python import Serializer
+from chat.constants import *
 from datetime import datetime
 
 def find_or_create_private_chat(user1, user2):
@@ -28,4 +30,18 @@ def calculate_timestamp(timestamp):
     else:
         str_time = datetime.strftime(timestamp, "%m/%d/%Y")
         ts = f"{str_time}"
-    return ts
+    return str(ts)
+
+class LaxyRoomChatMessageEncoder(Serializer):
+    def get_dump_object(self, obj):
+        dump_object = {}
+        dump_object.update({"msg_type": MSG_TYPE_MESSAGE})
+        dump_object.update({"msg_id": str(obj.id)})
+        dump_object.update({"user_id": str(obj.user.id)})
+        dump_object.update({"username": str(obj.user.username)})
+        dump_object.update({"message": str(obj.content)})
+        dump_object.update({"profile_image": str(obj.user.profile_image.url)})
+        dump_object.update({"natural_timestamp": calculate_timestamp(obj.timestamp)})
+        return dump_object
+        
+        
