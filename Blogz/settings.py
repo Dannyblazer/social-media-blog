d756309408bea319fd25fbb29a307b3872a2266c
Dannyblazer <nnajidanny004@gmail.com>
2023-04-08 07:39:51 +0000

    Production updates

--- Blogz/settings.py
+++ Blogz/settings.py
@@ -141,16 +141,17 @@ DATABASES = {
 ASGI_APPLICATION = 'Blogz.asgi.application'
 
 CHANNEL_LAYERS = {
-    'default': dj_redis_url.parse(os.environ.get("REDIS_URL"))
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("red-cgoa388u9tun42o97hdg", 6379)],
+        },
+    },
 }
 
 
 # Connect to your internal Redis instance using the REDIS_URL environment variable
 # The REDIS_URL is set to the internal Redis URL e.g. redis://red-343245ndffg023:6379
-r = redis.from_url(os.environ['REDIS_URL'])
-
-r.set('key', 'redis-py')
-r.get('key')
 
 
 # Password validation
@@ -216,4 +217,4 @@ else:
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 TEMP = os.path.join(BASE_DIR, 'media_cdn/temp')
-BASE_URL = 'http://' + ALLOWED_HOSTS[0]
+BASE_URL = str('http://' + ALLOWED_HOSTS[0])
