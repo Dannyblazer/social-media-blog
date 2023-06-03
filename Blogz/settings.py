58d85796b36fc992d586046810ed95f3a5dbc086
Dannyblazer <nnajidanny004@gmail.com>
2023-06-03 09:48:53 +0100

    Clear unncessary imports.

--- Blogz/settings.py
+++ Blogz/settings.py
@@ -9,12 +9,9 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-from django.utils import timezone
 from pathlib import Path
 import zoneinfo
-import redis
 import os
-import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
